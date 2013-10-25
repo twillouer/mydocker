a2a885d958bb38431ddef1d737494ce4ac1d1f98
William Delanoue <william.delanoue@deveryware.net>
2013-10-25 23:06:36 +0200

    right value for port

--- mongodb/mongodb.sh
+++ mongodb/mongodb.sh
@@ -2,11 +2,11 @@
 
 ## see http://docs.docker.io/en/latest/examples/mongodb/
 
-sudo docker build -t twillouer/mongodb - < mongodb.docker 
+sudo docker build -t twillouer/mongodb - < mongodb.docker || exit
 
 # Lean and mean
 MONGO_ID=$(sudo docker run -d twillouer/mongodb --noprealloc --smallfiles)
-PORT=$(sudo docker port $MONDO_ID)
+PORT=$(sudo docker port $MONGO_ID)
 
 echo ID : $MONGO_ID
 echo "##to connect : "
