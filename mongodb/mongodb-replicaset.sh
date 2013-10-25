18a90811435f987ec7c9237f52e19d5f5090b1da
William Delanoue <william.delanoue@gmail.com>
2013-10-26 01:26:43 +0200

    adding replica set

---
+++ mongodb/mongodb-replicaset.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## see http://docs.docker.io/en/latest/examples/mongodb/
+
+REPL_COUNT=${1:-3}
+
+sudo docker build -t twillouer/mongodb - < mongodb.docker || exit
+
+CONFIGURATE="rs.initiate() "
+
+# Lean and mean
+for i in $(seq 1 $REPL_COUNT) ; 
+do
+# --smallfiles --noprealloc --nojournal --oplogSize 10
+
+  MONGO_ID=$(sudo docker run -d twillouer/mongodb --noprealloc --smallfiles --oplogSize 10 --nojournal --replSet docker)
+  PORT=$(sudo docker port ${MONGO_ID} 27017)
+  # The primary will be the last 
+  PRIMARY_PORT=$PORT
+  IP=$(sudo docker inspect ${MONGO_ID} | grep IPAddress | cut -d '"' -f 4)
+  echo "Container n°$i : ${MONGO_ID} run on port ${PORT}" 
+  CONFIGURATE="$CONFIGURATE ; rs.add('${IP}:27017')"
+done
+
+while ! sudo docker logs $MONGO_ID | grep "waiting for connections on port 27017"
+do 
+ sleep 2
+done
+
+echo "Configuration with :"
+echo $CONFIGURATE
+
+echo $CONFIGURATE |  mongo --port $PRIMARY_PORT
+
+echo "##to connect : "
+echo "mongo --port $PRIMARY_PORT"
--- mongodb/mongodb.sh
+++ mongodb/mongodb.sh
@@ -6,7 +6,7 @@ sudo docker build -t twillouer/mongodb - < mongodb.docker || exit
 
 # Lean and mean
 MONGO_ID=$(sudo docker run -d twillouer/mongodb --noprealloc --smallfiles)
-PORT=$(sudo docker port $MONGO_ID)
+PORT=$(sudo docker port $MONGO_ID 27017)
 
 echo ID : $MONGO_ID
 echo "##to connect : "
