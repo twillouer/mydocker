2f560c7f3e6b5b745d384b33f2d1e0b6a658d588
William Delanoue <william.delanoue@gmail.com>
2013-10-30 00:38:58 +0100

    in the poche is now persistant

--- poche/yourpoche.sh
+++ poche/yourpoche.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
+DBPATH=${1:-db}
+
+
+# Full path
+DBPATH=$(readlink -f $DBPATH)
+
+# Create directory if not exist
+mkdir -p "$DBPATH"
+
+echo "Real storage will be in $DBPATH"
+
 # if you want to change the container name.
 CONTAINERNAME=poche
 
-sudo docker images $CONTAINERNAME | grep $CONTAINERNAME || sudo docker build -t $CONTAINERNAME . || exit
+if sudo docker images $CONTAINERNAME | grep -q $CONTAINERNAME ;
+then
+ :
+else
+ sudo docker build -t $CONTAINERNAME . || exit
+fi
 
-CONTAINER_ID=$(sudo docker run -d $CONTAINERNAME)
+if [ ! -f $DBPATH/poche.sqlite ] ;
+then
+ echo "#Export poche.sqlite from container image to filesystem, in $DBPATH"
+ sudo docker run --entrypoint="/bin/bash" $CONTAINERNAME -c "cat /var/www/db/poche.sqlite" > "$DBPATH/poche.sqlite" || exit
+fi
+
+sudo chmod 777 "$DBPATH" "$DBPATH/poche.sqlite"
+
+CONTAINER_ID=$(sudo docker run -d -v=$DBPATH:/var/www/db $CONTAINERNAME)
 PORT=$(sudo docker port $CONTAINER_ID 80)
 
 echo
 echo
-echo "Runing on port $PORT"
+echo "#Runing on port $PORT"
 echo "sensible-browser http://localhost:$PORT"
 echo
-echo "Stop with :"
+echo "#Stop with :"
 echo "sudo docker stop $CONTAINER_ID"
