315b588d3d8a3195cbc6f190741c8eb9c4552ea0
William Delanoue <william.delanoue@gmail.com>
2014-02-25 04:50:47 +0100

    add memcached

--- mongodb/mongodb-inmemory.sh
+++ mongodb/mongodb-inmemory.sh
@@ -3,14 +3,14 @@
 ## see http://docs.docker.io/en/latest/examples/mongodb/
 ## see http://java.dzone.com/articles/how-use-mongodb-pure-memory-db
 
-sudo docker build -t mongodb . || exit
+docker build -t mongodb . || exit
 
 TMPDIR=$(mktemp -d)
 sudo mount -t tmpfs -o size=16000M tmpfs $TMPDIR || exit
 
 # Lean and mean
-MONGO_ID=$(sudo docker run -d -p=27017 -v="$TMPDIR":/data/db mongodb --noprealloc --smallfiles --nojournal)
-PORT=$(sudo docker port $MONGO_ID 27017 | cut -d":" -f2)
+MONGO_ID=$(docker run -d -p=27017 -v="$TMPDIR":/data/db mongodb --noprealloc --smallfiles)
+PORT=$(docker port $MONGO_ID 27017 | cut -d":" -f2)
 
 echo ID : $MONGO_ID
 echo "##to connect : "
@@ -18,11 +18,11 @@ echo "mongo --port $PORT"
 
 # Check the logs out
 echo "##Check the logs : "
-echo "sudo docker logs $MONGO_ID"
+echo "docker logs $MONGO_ID"
 
 echo
 # When done
 echo "##When done : "
-echo "sudo docker stop $MONGO_ID"
-echo "sudo docker rm $MONGO_ID"
-echo "sudo umount $TMPDIR"
+echo "docker stop $MONGO_ID"
+echo "docker rm $MONGO_ID"
+echo "umount $TMPDIR"
