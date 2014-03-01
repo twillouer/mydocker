68bf0c16ad2e8ba7371930de38c65368a7f5617e
William Delanoue <william.delanoue@gmail.com>
2014-03-01 10:03:12 +0100

    add sonarqube

---
+++ sonarqube/sonarqube.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker build -rm -t sonarqube . || exit
+
+# Lean and mean
+ID=$(docker run -d -p=9000:9000 -p=9092:9092  sonarqube)
+PORT=$(docker port ${ID} 9000 | cut -d":" -f2)
+
+echo ID : $ID
+echo "##to connect : "
+echo "sensible-browser http://localhost:$PORT"
+
+# Check the logs out
+echo "##Check the logs : "
+echo "docker logs $ID"
