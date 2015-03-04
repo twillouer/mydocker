8562f2bd4a591687e2a16dca5417e999012ad55f
William Delanoue <william.delanoue@gmail.com>
2015-03-04 11:39:58 +0100

    correct

--- mongodb/mongodb-persist.sh
+++ mongodb/mongodb-persist.sh
@@ -7,12 +7,12 @@
 DATA=$(readlink -f ${1:-data})
 mkdir -p $DATA
 
-sudo docker --rm build -t mongodb . || exit
+docker build --rm -t mongodb . || exit
 
 # Lean and mean
-MONGO_ID=$(sudo docker run -p=127.0.0.1:27017${MONGO_PORT} -p=127.0.0.1:28017 -v=$DATA:/data/db -d mongodb --noprealloc --smallfiles --storageEngine wiredTiger)
-PORT=$(sudo docker port $MONGO_ID 27017 | cut -d":" -f2)
-WEBPORT=$(sudo docker port $MONGO_ID 28017 | cut -d":" -f2)
+MONGO_ID=$(docker run -p=127.0.0.1:27017${MONGO_PORT} -p=127.0.0.1::28017 -v=$DATA:/data/db -d mongodb --noprealloc --smallfiles --storageEngine wiredTiger)
+PORT=$(docker port $MONGO_ID 27017 | cut -d":" -f2)
+WEBPORT=$(docker port $MONGO_ID 28017 | cut -d":" -f2)
 
 echo ID : $MONGO_ID
 echo
@@ -27,4 +27,4 @@ echo
 
 # Check the logs out
 echo "##Check the logs : "
-echo "sudo docker logs $MONGO_ID"
+echo "docker logs $MONGO_ID"
