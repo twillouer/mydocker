01ab6c6642047ca3c1aae5e1633d641f14fb22a1
William Delanoue <william.delanoue@gmail.com>
2015-08-13 15:41:42 +0200

    mongodb

--- mongodb/mongodb-replicaset.sh
+++ mongodb/mongodb-replicaset.sh
@@ -4,7 +4,7 @@
 
 REPL_COUNT=${1:-3}
 
-sudo docker build --rm -t mongodb . || exit
+docker build --rm -t mongodb . || exit
 
 CONFIGURATE="rs.initiate() ; sleep(15000); "
 
@@ -13,19 +13,19 @@ for i in $(seq 1 $REPL_COUNT) ;
 do
 # --smallfiles --noprealloc --nojournal --oplogSize 10
 
-  MONGO_ID=$(sudo docker run -d -p=27017 mongodb --noprealloc --smallfiles --oplogSize 10 --nojournal --replSet docker) || exit
-  PORT=$(sudo docker port ${MONGO_ID} 27017 | cut -d":" -f2)
+  MONGO_ID=$(docker run -d -p=27017 mongodb --noprealloc --smallfiles --oplogSize 10 --nojournal --replSet docker) || exit
+  PORT=$(docker port ${MONGO_ID} 27017 | cut -d":" -f2)
   # The primary will be the last 
   PRIMARY_PORT=$PORT
-  IP=$(sudo docker inspect ${MONGO_ID} | grep IPAddress | cut -d '"' -f 4)
+  IP=$(docker inspect ${MONGO_ID} | grep IPAddress | cut -d '"' -f 4)
   echo "Container n°$i : ${MONGO_ID} run on port ${PORT}" 
   CONFIGURATE="$CONFIGURATE ; rs.add('${IP}:27017') ;"
 done
 
-while ! sudo docker logs $MONGO_ID | grep "waiting for connections on port 27017"
+while ! docker logs $MONGO_ID | grep "waiting for connections on port 27017"
 do 
 echo $MONGO_ID
- sudo docker logs $MONGO_ID
+ docker logs $MONGO_ID
  sleep 2
 done
 
