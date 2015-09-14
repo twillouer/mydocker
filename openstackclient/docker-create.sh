7fd78f001d6ec1840e72f4c8cf37e42a6fa707e9
William Delanoue <william.delanoue@gmail.com>
2015-09-14 22:15:34 +0200

    latest version

--- openstackclient/docker-create.sh
+++ openstackclient/docker-create.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
+#
+# Usage
+#  $0 <hostname> --swarm-master
+#  $0 <hostname>
+#
+#
+
+if [ -z "${1+x}" ] ;
+then
+  echo "Usage : $0" '<node-name> [options to docker-machine]'
+  echo 'options can be, for example --swarm-master'
+  echo "or --engine-insecure-registry $(hostname -I | cut -d" " -f1):5000"
+  exit
+fi
+
 if [ -z "${TOKEN+x}" ] ;
 then
- echo #TOKEN must be set
- echo 'TOKEN=$(docker run --rm swarm create)'
- echo export TOKEN
+ echo '#TOKEN must be set'
+ echo 'export TOKEN=$(docker run --rm swarm create)'
  exit 1
 fi
 
 IMAGE=${IMAGE:-ubuntu_docker_14.04}
 SSHUSER=${SSHUSER:-stack}
 FLAVOR=${FLAVOR:-2_vCPU_RAM_8G_HD_10G}
-NODENAME=swarm-node-${1:-00}
+NODENAME=${1}
 MASTER=
 
-if [ "${1}" == 'master' ] ;
-then
-   NODENAME=swarm-master
-   MASTER=--swarm-master
-   shift
-fi
+shift
 
+#if [ "${1}" == 'master' ] ;
+#then
+#   NODENAME=swarm-master
+#   MASTER=--swarm-master
+#   shift
+#fi
 
-docker-machine -D create --driver openstack --openstack-flavor-name ${FLAVOR} --openstack-image-name ${IMAGE} --openstack-net-name net-001 --openstack-ssh-user ${SSHUSER} --swarm ${MASTER} --swarm-discovery token://$TOKEN $NODENAME
+echo Create machine $NODENAME with options $*
+
+docker-machine -D create --driver openstack --openstack-flavor-name ${FLAVOR} --openstack-image-name ${IMAGE} --openstack-net-name net-001 --openstack-ssh-user ${SSHUSER} --swarm $* --swarm-discovery token://$TOKEN $NODENAME
 
 if [ "$?" != 0 ] ;
 then
@@ -32,5 +49,7 @@ fi
 
 if [ ! -z "$MASTER" ] ;
 then
-  echo 'eval $(docker-machine env --swarm' $NODENAME ')'
+  echo 'eval $(docker-machine env --swarm' $NODENAME')'
+else
+  echo 'eval $(docker-machine env' $NODENAME')'
 fi
